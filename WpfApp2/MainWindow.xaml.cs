665cebdd0e2fb0d2be64b59cc9e2e244f4e4c8a1
MarinaSubbotina2002 <95845646+MarinaSubbotina2002@users.noreply.github.com>
2021-12-12 11:24:34 +0700

    first commit

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        testEntities1 conn = new testEntities1();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Open_button_Click(object sender, RoutedEventArgs e)
+        {
+            int count = conn.Admin.Where(c => (c.Jobnumber == Username_label.Text) && (c.Passwords == Passwordbox.Password)).Count();
+
+            if(count==1)
+            {
+                var Role = (from c in conn.Admin
+                            where c.Jobnumber == Username_label.Text
+                            select c.RoleId).FirstOrDefault();
+                if (Role == "1")
+                {
+                    Window1 window1 = new Window1();
+                    window1.Show();
+                    this.Close();
+                }
+                if (Role == "2")
+                {
+                    Window2 window2 = new Window2();
+                    window2.Show();
+                    this.Close();
+                }
+            }
+        }
+    
+    }
+}
